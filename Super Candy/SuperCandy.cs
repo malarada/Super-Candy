c85d6e851819d7242c37a5e5c3c3c3c21c5d6d0c
Pyro Cyclone <cyclonepyro@gmail.com>
2024-09-28 23:33:19 -0600

    Changed plugin prefix

--- Super Candy/SuperCandy.cs
+++ Super Candy/SuperCandy.cs
@@ -7,7 +7,7 @@ namespace Super_Candy_Plugin
     public class SuperCandyPlugin : Plugin<Config>
     {
         public override string Name => "Super Candy Plugin";
-        public override string Prefix => "SCP";
+        public override string Prefix => Name;
         public override string Author => "PyroCyclone";
         public override Version Version => new Version(1, 0, 0);
         public override Version RequiredExiledVersion => new Version(8, 11, 0);
